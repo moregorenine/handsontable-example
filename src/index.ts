229b41098be72cdb43a50cf81bcc71c503978720
moregorenine <moregorenine@gmail.com>
2021-10-03 22:54:42 +0900

    complete quick-start.html

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import { formData } from './forms';
+import 'handsontable/dist/handsontable.full.min.css'
+// @ts-ignore
+import Handsontable from 'handsontable'
 
-const form = document.querySelector('form')!;
+const data = [
+  ["", "Ford", "Tesla", "Toyota", "Honda"],
+  ["2017", 10, 11, 12, 13],
+  ["2018", 20, 11, 14, 13],
+  ["2019", 30, 15, 12, 13]
+];
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const data = formData(form);
-  console.log(data);
+const container = document.getElementById('example');
+const hot = new Handsontable(container, {
+  data: data,
+  rowHeaders: true,
+  colHeaders: true,
+  filters: true,
+  dropdownMenu: true
 });
+
+// import { formData } from './forms';
+//
+// const form = document.querySelector('form')!;
+//
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const data = formData(form);
+//   console.log(data);
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,31 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
-  entry: './src/index.ts',
+  entry: './src/js/quick-start.ts',
   module: {
     rules: [
       {
         test: /\.ts$/,
         include: [path.resolve(__dirname, 'src')],
         use: 'ts-loader',
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          }
+        ],
+      },
     ]
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
   output: {
-    publicPath: 'public',
+    publicPath: '/',
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
